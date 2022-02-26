2aeace32ab9a90cc0b175a87aa46fbf21b87acbb
Andrew <32970146+dancing-rain@users.noreply.github.com>
2022-02-26 06:17:29 -0600

    all classes now (properly) encapsulated,using dictionary for data storage, and have beautified json output

--- scraper.py
+++ scraper.py
@@ -14,8 +14,6 @@ import json
 abs_path = os.path.abspath(__file__)
 dir_name = os.path.dirname(abs_path)
 os.chdir(dir_name)
-file_name = "scraper_output.txt"
-opened_file = open(file_name,"w")
 
 reddit = praw.Reddit( #instance of praw reddit for API access
     client_id = 'g1newHxnqEdQYH8vN8hSLw',
@@ -39,64 +37,6 @@ def GetUsernameInput(): #Check if inputted username is valid
         print("\nUsername not found, try again\n")
         return GetUsernameInput()
     return name;
-    
-def FindVoteDistribution(): 
-    to_return = "\nUser's top subreddits ranked by comment/submission upvotes (Out of last 198 interactions):\n"
-    active_subreddits_map = {}
-    #combine comments and submissions into dictionary format {sub name, upvote count} to easily organize subreddits and increment their upvote counts
-    for comments in user_comments_list:
-        sub_name = comments.subreddit.display_name
-        upvote_qty = comments.score
-        if sub_name in active_subreddits_map.keys():
-            active_subreddits_map[sub_name] = active_subreddits_map[sub_name] + upvote_qty
-        else:
-            active_subreddits_map[sub_name] = upvote_qty
-    for submissions in user_submissions_list:
-        sub_name = submissions.subreddit.display_name
-        upvote_qty = submissions.score
-        if sub_name in active_subreddits_map.keys():
-            active_subreddits_map[sub_name] = active_subreddits_map[sub_name] + upvote_qty
-        else:
-            active_subreddits_map[sub_name] = upvote_qty
-    #convert map back to list, then use built-in triple parameter sort method to sort subreddits by upvote count
-    active_subreddits_list = []
-    for i,(k, v) in enumerate(active_subreddits_map.items()):
-        active_subreddits_list.append([k, v])
-    descending_subreddit_by_activity = sorted(active_subreddits_list,key=lambda x:x[1], reverse=True)
-    idx = 0
-    #print subreddit upvote distribution in descending order
-    for subreddit in descending_subreddit_by_activity:
-        to_return += str(idx+1) + ")\t" + "r/" + subreddit[0] + " | " + str(subreddit[1]) + " vote(s)\n"
-        idx+=1
-    return to_return
-    
-def FindMostActive():
-    to_return = "\nTop active subreddits ranked by quantity of comments and submissions (Out of last 198 interactions):\n"
-    active_subreddits_map = {}
-    #combine comments and submissions into dictionary format {sub name, upvote count} to easily organize subreddits and increment their interaction count
-    for comments in user_comments_list:
-        sub_name = comments.subreddit.display_name
-        if sub_name in active_subreddits_map.keys():
-            active_subreddits_map[sub_name] = active_subreddits_map[sub_name] + 1
-        else:
-            active_subreddits_map[sub_name] = 1
-    for submissions in user_submissions_list:
-        sub_name = submissions.subreddit.display_name
-        if sub_name in active_subreddits_map.keys():
-            active_subreddits_map[sub_name] = active_subreddits_map[sub_name] + 1
-        else:
-            active_subreddits_map[sub_name] = 1
-    #convert map back to list, then use built-in triple parameter sort method to sort subreddits by upvote count
-    active_subreddits_list = []
-    for i,(k, v) in enumerate(active_subreddits_map.items()):
-        active_subreddits_list.append([k, v])
-    descending_subreddit_by_activity = sorted(active_subreddits_list,key=lambda x:x[1], reverse=True)
-    idx = 0
-    #print subreddit interactions in descending order
-    for subreddit in descending_subreddit_by_activity:
-        to_return += str(idx+1) + ")\t" + "r/" + subreddit[0] + " | " + str(subreddit[1]) + " interaction(s)\n"
-        idx+=1
-    return to_return
 
 class UserInfo:
     id: str #user's id - short series of alphanumeric charaacters
@@ -146,9 +86,9 @@ class UserInfo:
             self.karma_comments = str(user_as_redditor.comment_karma) + " karma"
             self.karma_overall = str(user_as_redditor.link_karma + user_as_redditor.comment_karma) + " karma"
             #Is user a moderator
-            self.moderator = False;
+            self.moderator = "False";
             if (user_as_redditor.is_mod):
-                self.moderator = True;
+                self.moderator = "True";
             self.info_map = {"Username":self.name, "Cake Day":self.cake_day, "Age":self.age, "User Comment Karma":self.karma_comments, "User Overall Karma":self.karma_overall, "User is a moderator":self.moderator, "User is suspended":self.suspended, "User ID":self.id}
 
             
@@ -156,109 +96,223 @@ class UserInfo:
         return self.suspended == "True"
     
     def ConvertBasicInfoToTxt(self):
+        with open("scraper_output.json", "r") as f:   
+            feed = json.load(f) 
         with open("scraper_output.json", "w") as outfile:
-            json.dump(self.info_map, outfile)
+            feed.append({"UserInfo":self.info_map})
+            json.dump(list(feed), outfile, indent=2)
         
     def PrintBasicInfo(self):
         for i,(k,v) in enumerate(self.info_map.items()):
             print(str(k) + ": " + str(v))
-            
-        
 class TopFiveVotedSubmissionsData:
     descriptive_header: str
-    info_map: map
+    info_list_of_maps: list
     
     def __init__(self, descriptive_header="\nTop 5 most upvoted posts (Out of last 99 posts):\n", txt_delimiter = "TopFiveVotedSubmissionsData_delim"):
         self.descriptive_header = descriptive_header
-        self.info_map = {"Rank":"", "Score":"","Time:":"", "Comments":"", "Title":""}
+        self.info_list_of_maps = []
         
     def FindFiveMostVotedSubmissions(self):
         sorted_submissions = sorted(user_submissions_list,key=lambda x:x.score, reverse=True)
         idx = 0
         for submission in sorted_submissions:
             if idx < 5 and idx < len(sorted_submissions):
-                self.info_values.append([idx + 1, submission.score, datetime.utcfromtimestamp(int(submission.created_utc)).strftime("%m/%d/%Y, %H:%M:%S"), submission.num_comments, submission.title])
+                self.info_list_of_maps.append({"Rank":str(idx + 1), "Score":str(submission.score),"Time:":str(datetime.utcfromtimestamp(int(submission.created_utc)).strftime("%m/%d/%Y, %H:%M:%S")), "Comments":str(submission.num_comments), "Title":str(submission.title)})
             idx+=1
             
     def PrintFiveMostVotedSubmissions(self):
         print(self.descriptive_header)
-        for idx in range(0,len(self.info_values)):
+        for idx in range(0,len(self.info_list_of_maps)):
             to_print = ""
-            for idx1 in range(0,len(self.info_keys)):
-                to_print += self.info_keys[idx1] + str(self.info_values[idx][idx1])
+            for idx1,(k,v) in enumerate(self.info_list_of_maps[idx].items()):
+                to_print += str(k) + ": " + str(v)
+                if idx1 < len(self.info_list_of_maps[idx]):
+                    to_print += " | "
             print(to_print)
             
     def ConvertFiveMostVotedSubmissionsToTxt(self):
-        opened_file.write("\n" + self.txt_delimiter + "\n")
-        for idx in range(0,len(self.info_values)):
-            to_txt = "listbegin_delim"
-            for idx1 in range(0,len(self.info_keys)):
-                to_txt +=str(self.info_values[idx][idx1]) + ";,."
-            to_txt += "list_delim_close\n"
-            opened_file.write(to_txt)
-        opened_file.write(self.txt_delimiter + "_close\n")
-        
+        with open("scraper_output.json", "r") as f:   
+            feed = json.load(f) 
+        with open("scraper_output.json", "w") as outfile:
+            info_map = {}
+            for i in range(0,len(self.info_list_of_maps)):
+                submission_map = {}
+                for idx, (k,v) in enumerate(self.info_list_of_maps[i].items()):
+                    submission_map[k] = v
+                info_map.update({i+1:submission_map.copy()})
+            to_append = {"FiveMostVotedSubmissions":info_map}
+            feed.append(to_append)
+            json.dump(list(feed), outfile, indent=2)
 class TopFiveVotedCommentsData:
     descriptive_header: str
-    info_keys: list
-    info_values: list
-    txt_delimiter: str
+    info_list_of_maps: list
     def __init__(self, descriptive_header="\nTop 5 most upvoted comments (Out of last 99 posts):\n", txt_delimiter = "TopFiveVotedCommentsData_delim"):
         self.descriptive_header = descriptive_header
-        self.info_keys = ["Rank ", "| Score: ", " | Time: ", " UTC | ", " replies | Contents: "]
-        self.info_values = []
-        self.txt_delimiter = txt_delimiter
+        self.info_list_of_maps = []
         
     def FindFiveMostVotedComments(self):
         sorted_comments = sorted(user_comments_list,key=lambda x:x.score, reverse=True)
         idx = 0
         for comments in sorted_comments:
             if idx < 5 and idx < len(sorted_comments):
-                self.info_values.append([idx + 1, comments.score, datetime.utcfromtimestamp(int(comments.created_utc)).strftime("%m/%d/%Y, %H:%M:%S"), len(comments.replies), comments.body.replace("\n","")[0:69]])
+                self.info_list_of_maps.append({"Rank":str(idx+1),"Score":str(comments.score), "Time":str(datetime.utcfromtimestamp(int(comments.created_utc)).strftime("%m/%d/%Y, %H:%M:%S")), "Replies":str(len(comments.replies)), "Body":(comments.body.replace("\n","")[0:35]+"...")})
             idx+=1
         
     def PrintFiveMostVotedComments(self):
         print(self.descriptive_header)
-        for idx in range(0,len(self.info_values)):
+        for idx in range(0,len(self.info_list_of_maps)):
             to_print = ""
-            for idx1 in range(0,len(self.info_keys)):
-                to_print += self.info_keys[idx1] + str(self.info_values[idx][idx1])
+            for idx1,(k,v) in enumerate(self.info_list_of_maps[idx].items()):
+                to_print += str(k) + ": " + str(v)
+                if idx1 < len(self.info_list_of_maps[idx]):
+                    to_print += " | "
             print(to_print)
             
     def ConvertFiveMostVotedCommentsToTxt(self):
-        opened_file.write("\n" + self.txt_delimiter + "\n")
-        for idx in range(0,len(self.info_values)):
-            to_txt = "listbegin_delim"
-            for idx1 in range(0,len(self.info_keys)):
-                to_txt +=str(self.info_values[idx][idx1]) + ";,."
-            to_txt += "list_delim_close\n"
-            opened_file.write(to_txt)
-        opened_file.write(self.txt_delimiter + "_close\n")
+        with open("scraper_output.json", "r") as f:   
+            feed = json.load(f) 
+        with open("scraper_output.json", "w") as outfile:
+            info_map = {}
+            for i in range(0,len(self.info_list_of_maps)):
+                submission_map = {}
+                for idx, (k,v) in enumerate(self.info_list_of_maps[i].items()):
+                    submission_map[k] = v
+                info_map.update({i+1:submission_map.copy()})
+            to_append = {"FiveMostVotedSubmissions":info_map}
+            feed.append(to_append)
+            json.dump(list(feed), outfile, indent=2)
 class VoteDistribution:
     descriptive_header: str
-    txt_delimiter: str
+    info_list_of_maps: list
     def __init__(self, descriptive_header="\nUser's top subreddits ranked by comment/submission upvotes (Out of last 198 interactions):\n", txt_delimiter = "VoteDistribution_delim"):
         self.descriptive_header = descriptive_header
-        self.txt_delimiter = txt_delimiter
+        self.info_list_of_maps = []
+        
+    def FindVoteDistribution(self): 
+        active_subreddits_map = {}
+        #combine comments and submissions into dictionary format {sub name, upvote count} to easily organize subreddits and increment their upvote counts
+        for comments in user_comments_list:
+            sub_name = comments.subreddit.display_name
+            upvote_qty = comments.score
+            if sub_name in active_subreddits_map.keys():
+                active_subreddits_map[sub_name] = active_subreddits_map[sub_name] + upvote_qty
+            else:
+                active_subreddits_map[sub_name] = upvote_qty
+        for submissions in user_submissions_list:
+            sub_name = submissions.subreddit.display_name
+            upvote_qty = submissions.score
+            if sub_name in active_subreddits_map.keys():
+                active_subreddits_map[sub_name] = active_subreddits_map[sub_name] + upvote_qty
+            else:
+                active_subreddits_map[sub_name] = upvote_qty
+        #convert map back to list, then use built-in triple parameter sort method to sort subreddits by upvote count
+        active_subreddits_list = []
+        for i,(k, v) in enumerate(active_subreddits_map.items()):
+            active_subreddits_list.append([k, v])
+        descending_subreddit_by_activity = sorted(active_subreddits_list,key=lambda x:x[1], reverse=True)
+        idx = 0
+        #print subreddit upvote distribution in descending order
+        for subreddit in descending_subreddit_by_activity:
+            self.info_list_of_maps.append({"Rank":str(idx+1),"Subreddit":subreddit[0], "Vote Count":str(subreddit[1])})
+            idx+=1
+            
+    def PrintVoteDistribution(self):
+        print(self.descriptive_header)
+        for idx in range(0,len(self.info_list_of_maps)):
+            to_print = ""
+            for idx1,(k,v) in enumerate(self.info_list_of_maps[idx].items()):
+                to_print += str(k) + ": " + str(v)
+                if idx1 < len(self.info_list_of_maps[idx]):
+                    to_print += " | "
+            print(to_print)
+            
+    def ConvertVoteDistributionToTxt(self):
+        with open("scraper_output.json", "r") as f:   
+            feed = json.load(f) 
+        with open("scraper_output.json", "w") as outfile:
+            info_map = {}
+            for i in range(0,len(self.info_list_of_maps)):
+                submission_map = {}
+                for idx, (k,v) in enumerate(self.info_list_of_maps[i].items()):
+                    submission_map[k] = v
+                info_map.update({i+1:submission_map.copy()})
+            to_append = {"VoteDistribution":info_map}
+            feed.append(to_append)
+            json.dump(list(feed), outfile, indent=2)
 class MostActiveSubs:
     descriptive_header: str
-    txt_delimiter: str
+    info_list_of_maps: list
     def __init__(self, descriptive_header="\nTop active subreddits ranked by quantity of comments and submissions (Out of last 198 interactions):\n", txt_delimiter = "MostActiveSubs_delim"):
         self.descriptive_header = descriptive_header
-        self.txt_delimiter = txt_delimiter
+        self.info_list_of_maps = []
+        
+    def FindMostActive(self):
+        active_subreddits_map = {}
+        #combine comments and submissions into dictionary format {sub name, upvote count} to easily organize subreddits and increment their interaction count
+        for comments in user_comments_list:
+            sub_name = comments.subreddit.display_name
+            if sub_name in active_subreddits_map.keys():
+                active_subreddits_map[sub_name] = active_subreddits_map[sub_name] + 1
+            else:
+                active_subreddits_map[sub_name] = 1
+        for submissions in user_submissions_list:
+            sub_name = submissions.subreddit.display_name
+            if sub_name in active_subreddits_map.keys():
+                active_subreddits_map[sub_name] = active_subreddits_map[sub_name] + 1
+            else:
+                active_subreddits_map[sub_name] = 1
+        #convert map back to list, then use built-in triple parameter sort method to sort subreddits by upvote count
+        active_subreddits_list = []
+        for i,(k, v) in enumerate(active_subreddits_map.items()):
+            active_subreddits_list.append([k, v])
+        descending_subreddit_by_activity = sorted(active_subreddits_list,key=lambda x:x[1], reverse=True)
+        idx = 0
+        #print subreddit interactions in descending order
+        for subreddit in descending_subreddit_by_activity:
+            self.info_list_of_maps.append({"Rank":str(idx+1),"Subreddit":subreddit[0], "Post/Repl Count":str(subreddit[1])})
+            idx+=1
+    
+    def PrintActiveSubs(self):
+        print(self.descriptive_header)
+        for idx in range(0,len(self.info_list_of_maps)):
+            to_print = ""
+            for idx1,(k,v) in enumerate(self.info_list_of_maps[idx].items()):
+                to_print += str(k) + ": " + str(v)
+                if idx1 < len(self.info_list_of_maps[idx]):
+                    to_print += " | "
+            print(to_print)
+            
+    def ConvertActiveSubsToTxt(self):
+        with open("scraper_output.json", "r") as f:   
+            feed = json.load(f) 
+        with open("scraper_output.json", "w") as outfile:
+            info_map = {}
+            for i in range(0,len(self.info_list_of_maps)):
+                submission_map = {}
+                for idx, (k,v) in enumerate(self.info_list_of_maps[i].items()):
+                    submission_map[k] = v
+                info_map.update({i+1:submission_map.copy()})
+            to_append = {"MostActiveSubreddits":info_map}
+            feed.append(to_append)
+            json.dump(list(feed), outfile, indent=2)
 
 if __name__ == '__main__':
     print()
     user_name = GetUsernameInput()
     print()
     
+    with open("scraper_output.json", mode='w') as outfile:
+        json.dump([], outfile, indent=2)
+        
     user_as_redditor = reddit.redditor(user_name)
     user_info = UserInfo()
     
-    user_comments_list = list(user_as_redditor.comments.new(limit=99)) #Limited to 100 historical submissions by Reddit API
-    user_submissions_list = list(user_as_redditor.submissions.new(limit=99)) #Limited to 100 historical submissions by Reddit API
+    user_comments_list = list(user_as_redditor.comments.new(limit=99)).copy() #Limited to 100 historical submissions by Reddit API
+    user_submissions_list = list(user_as_redditor.submissions.new(limit=99)).copy() #Limited to 100 historical submissions by Reddit API
     
-    if user_info.IsSuspended():
+    
+    if user_info.IsSuspended(): #todo issuspended status needs to be updated accurately prior
         print("User is shadowbanned - only contains name and is_suspended attributes")
     else:
         user_info.SetBasicInfo()
@@ -275,8 +329,13 @@ if __name__ == '__main__':
         u2.PrintFiveMostVotedComments()
         u2.ConvertFiveMostVotedCommentsToTxt()
         
-        print(FindVoteDistribution())
+        u3 = VoteDistribution()
+        u3.FindVoteDistribution()
+        u3.PrintVoteDistribution()
+        u3.ConvertVoteDistributionToTxt()
         
-        print(FindMostActive())
+        u4 = MostActiveSubs()
+        u4.FindMostActive()
+        u4.PrintActiveSubs()
+        u4.ConvertActiveSubsToTxt()
     print("")
-    opened_file.close()
