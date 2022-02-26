f7d4669de63c3bbaed3cc5f442c77b199d30c05b
Christian Classen <christian@cclassen.com>
2022-02-26 02:00:39 -0600

    add bot.py and starting code

---
+++ bot.py
@@ -0,0 +1,24 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv("./.env")
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} We good')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'ping':
+        await message.channel.send('seal so sexy')
+
+client.run(TOKEN)
