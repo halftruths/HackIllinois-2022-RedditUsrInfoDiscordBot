035849177cd7091f0ffeecee44e1ae0f9e3fc13f
Christian Classen <christian@cclassen.com>
2022-02-27 03:38:54 -0600

    Implement help, activitychart, and popchart

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 from email.mime import image
 import os
+from traceback import clear_frames
 
 import praw
 import discord
@@ -18,16 +19,15 @@ reddit = praw.Reddit( #instance of praw reddit for API access
         user_agent = os.getenv("USER_AGENT"),
     )
 
-class redditWrapper:
+class RedditWrapper:
     user_info: scraper.UserInfo
     user_comments_list: list
     user_submissions_list: list
 
-    def __init__(info: scraper.UserInfo):
-        user_info = info
-        user_as_redditor = reddit.redditor(user_info.name)
-        user_comments_list = list(user_as_redditor.comments.new(limit=99)).copy() #Limited to 100 historical submissions by Reddit API
-        user_submissions_list = list(user_as_redditor.submissions.new(limit=99)).copy() #Limited to 100 historical submissions by Reddit API
+    def __init__(self, username : str):
+        user_as_redditor = reddit.redditor(username)
+        self.user_comments_list = list(user_as_redditor.comments.new(limit=99)).copy() #Limited to 100 historical submissions by Reddit API
+        self.user_submissions_list = list(user_as_redditor.submissions.new(limit=99)).copy() #Limited to 100 historical submissions by Reddit API
 # Dict of usernames to reddit wrapper object
 username_dict = dict()
 
@@ -59,7 +59,8 @@ async def on_message(message):
             await message.channel.send(file=file, embed=embed)
 
         if command_name == 'help':
-            embedH = discord.Embed(title="Help Commands", description="Here is a list of commands available with this bot.")
+            embed = discord.Embed(title="Help Commands", description="Here is a list of commands available with this bot: \n r!ping - responds \"pong\" (connection check) \n r!stats <username> - shows basic user statistics \n r!all <username> - shows all data scraped from reddit \n r!popchart <username> - embeds a piechart of the user's upvote distribution \n r!activitychart <username> - embeds a piechart of user's activity distribution \nr!topcomments <username> - shows the user's top five voted comments \nr!topposts <username> - shows the user's top five voted posts")
+            await message.channel.send(embed=embed)
             
         if command_name == 's':
             user_as_redditor = scraper.reddit.redditor(command[2][0:2])
@@ -69,23 +70,57 @@ async def on_message(message):
                     add_user_to_dict()
                 
                 embedU = discord.Embed(title="User Information", description="")
-                help_func()
+                await message.channel.send(embed=embed)
 
         if command_name == "popchart":
+            if (len(command) == 1):
+                await message.channel.send("Error: No username inputted.")
+                return
+
             username = command[1]
             if (not is_user_in_dict(username=username)):
-                if (scraper.UserExists(username)):
-                    add_user_to_dict()
+                if (scraper.UserExists(username, reddit=reddit)):
+                    add_user_to_dict(username)
+                else:
+                    await message.channel.send("Error: Invalid username inputted.")
+            user = username_dict[username]
+            distribution = scraper.VoteDistribution()
+            distribution.FindVoteDistribution(user.user_comments_list, user.user_submissions_list)
+            dist_list, labels = distribution.GetDistributionAsList()
+            plot = plt.pie(dist_list[0:5], labels=labels[0:5], shadow=True, autopct='%1.1f%%', startangle=90)
+            plt.title("Upvote Distribution")
+            plt.savefig("piechart")
+            plt.clf()
+            embed = make_embed("Top 5 Subreddits by Upvotes", "For u/" + username)
+            file = discord.File("piechart.png", filename="image.png")
+            await message.channel.send(file=file, embed=embed)
+
 
         if command_name == "activitychart":
-            return
+            username = command[1]
+            if (not is_user_in_dict(username=username)):
+                if (scraper.UserExists(username, reddit=reddit)):
+                    add_user_to_dict(username)
+                else:
+                    await message.channel.send("Error: Invalid username inputted.")
+            user = username_dict[username]
+            most_subs = scraper.MostActiveSubs()
+            most_subs.FindMostActive(user.user_comments_list, user.user_submissions_list)
+            subs_list, labels = most_subs.GetActiveSubsAsList()
+            plot = plt.pie(subs_list[0:5], labels=labels[0:5], shadow=True, autopct='%1.1f%%', startangle=90)
+            plt.title("Post/Reply Distribution")
+            plt.savefig("piechart")
+            plt.clf()
+            embed = make_embed("Top 5 Subreddits by Posts/Replies", "For u/" + username)
+            file = discord.File("piechart.png", filename="image.png")
+            await message.channel.send(file=file, embed=embed)
 
 
 def is_user_in_dict(username: str):
-    return username_dict.has_key(str)
+    return username in username_dict.keys()
 
-def add_user_to_dict(user:UserInfo):
-    redditWrapper()
+def add_user_to_dict(username: str):
+    username_dict[username] = RedditWrapper(username)
 
 def make_embed(embed_title, description):
     embed=discord.Embed(title=embed_title, type="rich", description=description,  color=discord.Color.blue())
