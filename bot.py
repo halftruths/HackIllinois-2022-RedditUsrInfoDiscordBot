16aea31e78116ae7c3bce12857add324ebc17464
Udit Pai <upai3@illinois.edu>
2022-02-26 21:45:00 -0600

    merge

--- bot.py
+++ bot.py
@@ -12,19 +12,39 @@ PREFIX = "r!"
 
 client = discord.Client()
 
+
+def help_func():
+    return
+
 @client.event
 async def on_ready():
     print(f'{client.user} We good')
 
 @client.event
 async def on_message(message):
-    command = message.content.strip().split()
+    command = message.content.strip().split(" ")
     if (command[0][0:2] == PREFIX):
-        command_name = command[0][2:]
+        command_name = command[1][0:]
         if message.author == client.user:
             return
 
         if command_name == 'ping':
             await message.channel.send('pong')
+
+        if command_name == 'help':
+            embedH = discord.Embed(title="Help Commands", description="Here is a list of commands available with this bot.")
+            
+            
+
+        if command_name == 's':
+            user_as_redditor = scrape.reddit.redditor(command[2][0:2])
+            user_info = scrape.UserInfo()
+            if (scrape.UserExists(command[2][0:])):
+                embedU = discord.Embed(title="User Information", description="")
+                help_func()
+
+        if command_name == 'test': 
+            await message.channel.send('tester')
+
 
 client.run(TOKEN)
