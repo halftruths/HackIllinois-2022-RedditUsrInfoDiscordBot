ba06c61d084589b2c293a93f7fb246b851223241
Christian Classen <christian@cclassen.com>
2022-02-26 18:11:19 -0600

    Added functionality for sending embeds

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 # bot.py
+from email.mime import image
 import os
+
 
 import discord
 from dotenv import load_dotenv
 import scraper as scrape
+import matplotlib.pyplot as plt
 
 load_dotenv("./.env")
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,7 +21,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    command = message.content.strip().split()
+    command = message.content.split()
+    if (len(command) == 0 or len(command[0]) < 3):
+        return
     if (command[0][0:2] == PREFIX):
         command_name = command[0][2:]
         if message.author == client.user:
@@ -26,5 +31,16 @@ async def on_message(message):
 
         if command_name == 'ping':
             await message.channel.send('pong')
+
+        if command_name == 'test':
+            embed = make_embed("My Fellow Americans", "According to the 2020 US census")
+            file = discord.File("baby.png", filename="image.png")
+            await message.channel.send(file=file, embed=embed)
+
+
+def make_embed(embed_title, description):
+    embed=discord.Embed(title=embed_title, type="rich", description=description, color=discord.Color.blue())
+    embed.set_image(url="attachment://image.png")
+    return embed
 
 client.run(TOKEN)
